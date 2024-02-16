7f8f885faa5a9dfc641196995c618cf896063dbf
losif63 <jaduksuh@naver.com>
2024-02-16 04:47:56 +0000

    Rename fp8 and fix init file

---
+++ src/fp8/__init__.py
@@ -0,0 +1,3 @@
+from .quantize import fp8e4m3_matmul, fp8e5m2_matmul
+
+__all__ = ['fp8e4m3_matmul', 'fp8e5m2_matmul']
---
+++ src/msfp/__init__.py
@@ -0,0 +1,3 @@
+from .quantize import fp32_to_msfp16, msfp16_matmul
+
+__all__ = ['fp32_to_msfp16', 'msfp16_matmul']
